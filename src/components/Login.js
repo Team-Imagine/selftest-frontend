ec7b915cd6b31f41bd55c64fd4d66beb7fa61575
임선아 <suna32580@kakao.com>
2020-10-11 03:38:00 +0900

    Linking pages to Login

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import App from '../App';
+
+class Login extends React.Component {
+ render(){
+     
+     return(
+        <h1> This is the Logincomponent.</h1>
+         
+     )
+ }
+}
+export default Login;
