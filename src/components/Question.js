1f6d8b08819960e78730235e305de66a6744a08e
jinsungmin <jinsm807@gmail.com>
2020-11-21 14:08:12 +0900

    feat: make test in question page

--- src/components/Question.js
+++ src/components/Question.js
@@ -13,15 +13,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Question = ({ subject, course, isOpen }) => {
 
 	const [question, setQuestion] = useState([]);
-	/*
-	const [likeList, setLikeList] = useState([]);
-	const [dislikeList, setDislikeList] = useState([]);
-	const [freshList, setFreshList] = useState([]);
-	const [difficutlyList, setDifficultyList] = useState([]);
-	*/
 	const [pages, setPages] = useState([]);
-	let history = useHistory();
+	const [curPage, setCurPage] = useState(1);
 
+	const [temp, setTemp] = useState(true);
+	const [testState, setTestState] = useState("info");
+	const [selected, setSelected] = useState([]);
+	const [addTest, setAddTest] = useState([]);
+	
+	let history = useHistory();
+	
 	useEffect(() => {
 		axios.get(`/api/question?course_title=${course}`)
 			.then(res => {
@@ -30,35 +31,34 @@ const Question = ({ subject, course, isOpen }) => {
 				setQuestion(res.data.questions.rows);
 
 				let count = res.data.questions.count / 10 + 1;
+				let t_row = [];
+				let t_col = [];
+
+				for (var i = 1; i <= count; i++) {
+					t_col = [];
+					for(var j = 0; j< 10; j++) {
+						if((i-1) * 10 + j < res.data.questions.count) {
+							t_col.push({selected:"info", question_id: res.data.questions.rows[j].id});
+						}
+					}
+					t_row.push(t_col);
+				}
+
+				console.log(t_row);
+				setSelected(t_row);
+				
 				let t_pages = [];
 
 				for (var i = 1; i < count; i++) {
 					t_pages.push(i);
 				}
 				setPages(t_pages);
-
-				/*
-				let like_list = [];
-				let dislike_list = [];
-				let fresh_list = [];
-				let difficulty_list = [];
-				for(var i in res.data.questions) {
-					like_list.push(res.data.questions[i]['likeable_entity.total_likes']);
-					dislike_list.push(res.data.questions[i]['likeable_entity.total_dislikes']);
-					fresh_list.push(res.data.questions[i]['average_freshness']);
-					difficulty_list.push(res.data.questions[i]['average_difficulty']);
-				}
-				setLikeList(like_list);
-				setDislikeList(dislike_list);
-				setFreshList(fresh_list);
-				setDifficutlyList(difficultyList);
-				*/
 			})
 			.catch(error => {
 				alert(error.response.data.message);
 			})
 	}, []);
-
+	
 	const submitHandler = (event) => {
 		event.preventDefault();
 
@@ -81,12 +81,56 @@ const Question = ({ subject, course, isOpen }) => {
 			}
 		})
 			.then(res => {
-				console.log(res.data);
+				
+				let tSelected = selected;
+				for(var col = 0; col < res.data.questions.rows.length; col++) {
+					tSelected[index-1][col].question_id = res.data.questions.rows[col].id;
+				}
+				
+				console.log("test:", selected);
+				
 				setQuestion(res.data.questions.rows);
+				setSelected([...tSelected]);
+				setCurPage(index);
 			})
 			.catch(error => {
 				alert(error.response.data.message);
 			})
+			
+	}
+
+	const selectHandler = (index, e) => {
+		e.preventDefault();
+
+		let t_selected = selected;
+		if(t_selected[curPage -1][index].selected === "info")
+			t_selected[curPage -1][index].selected = "danger";
+		else {
+			t_selected[curPage -1][index].selected = "info";
+		}
+
+		setSelected([...t_selected]);
+	}
+
+	const makeTestHandler = () => {
+		if(testState === "info")
+			setTestState("danger");
+		else {
+			var result = window.confirm('시험을 생성하시겠습니까?');
+			if(result) {
+				console.log(selected);
+				
+				for(var row = 0; row < selected.length; row++) {
+					for(var col = 0; col < selected[row].length; col++) {
+					if(selected[row][col].selected === 'danger')
+						console.log(row , col);
+						console.log(question)
+					}
+				}
+			} else {
+				setTestState("info");
+			}
+		}
 	}
 
 	return (
@@ -103,6 +147,10 @@ const Question = ({ subject, course, isOpen }) => {
 					</div>
 					<div className="p-2 bd-highlight">
 						<div>
+						
+							<Button variant={testState} style = {{width: '19rem', height: '2.5rem'}} onClick={makeTestHandler}
+							>시험 생성</Button>
+							&nbsp;
 							<Button variant="info" style={{ width: '19rem', height: '2.5rem' }} onClick={submitHandler}
 								href={`/subject/${subject}/${course}/make/${1}`}
 							>문제 생성</Button>
@@ -112,14 +160,13 @@ const Question = ({ subject, course, isOpen }) => {
 				<hr />
 				<ul>
 					<CardDeck>
-						{question.map((i) =>
+						{question.map((i, index) =>
 							<div className="container h-100" key={i.id}>
 								<div className="row h-100 justify-content-center align-items-center">
 
 									<Card className="text-center" variant="info" style={{ width: '30rem' }}>
 										<Link key={i.id}
 											to={{
-												//pathname: `/subject/${subject}/${course}/${i.id}`,
 												pathname: `/subject/${subject}/${course}/${i.id}/problem_solving/${1}`
 											}}>
 											<Card.Header>
@@ -164,6 +211,14 @@ const Question = ({ subject, course, isOpen }) => {
 											</Card.Footer>
 										</Link>
 									</Card>
+									{(testState === "danger") &&
+									<div>
+										<Button
+										variant={selected[curPage -1][index].selected}
+										onClick={(e) => selectHandler(index, e)}
+										>추가</Button>
+									</div>
+									}
 								</div>
 								<br />
 							</div>
@@ -173,7 +228,7 @@ const Question = ({ subject, course, isOpen }) => {
 				<ul className="row justify-content-center align-items-center">
 					{pages.map((i, index) =>
 						<div key={index}>
-							<button style={{ backgroundColor: '#ffffff', border: '1px solid', width: '1.5rem' }} onClick={(e) => loadQuestionPerPage(i, e)}>{i}</button>
+							<button style={{ backgroundColor: '#ffffff', border: '1px solid', width: '1.5rem' }} onClick={(e) => loadQuestionPerPage(i, e)}>{i}</button>&nbsp;
 						</div>)
 					}
 				</ul>
